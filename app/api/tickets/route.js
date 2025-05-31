17f48eb9404c12784c443dda15bae6225c0f59f6
Jay1570 <jaygheewala157@gmail.com>
2025-05-31 10:19:11 +0530

    Refactor code formatting and improve consistency across components and configuration files

--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -8,7 +8,7 @@ export async function POST(req) {
     await Ticket.create(ticketData);
     return NextResponse.json({ message: "Ticket Created" }, { status: 201 });
   } catch (err) {
-    console.log(error);
+    console.log(err);
     return NextResponse.json({ message: "Error", err }, { status: 500 });
   }
 }
@@ -18,7 +18,7 @@ export async function GET(req) {
     const tickets = await Ticket.find();
     return NextResponse.json({ tickets }, { status: 200 });
   } catch (err) {
-    console.log(error);
+    console.log(err);
     return NextResponse.json({ message: "Error", err }, { status: 500 });
   }
 }
